f645106a2dfe8eccc1c78cdc26ee0062a87d9c5d
DAEHYEONJO <68371979+DAEHYEONJO@users.noreply.github.com>
2020-10-25 00:18:20 +0900

    ??

--- 맛의KU리/Manager.h
+++ 맛의KU리/Manager.h
@@ -5,16 +5,23 @@ class Manager
 {
 public:
 	vector<User> user;
-	User current_user;
+	User* current_user;
+	string* V_address;
 	int signUp();
 	int logIn();
 	void loginMenu();
+	bool yesorno();
 	int mainMenu();
 	void registerRestaurant();
 	void searchRestaurant();
 	void readInfoTextFile();
+	bool isin0to128(string str);
 	bool isQuit(string str);
 	bool isSpace(string str);
+	bool isTwoSlash(string s);
+	static void ltrim(string& s);
+	static void rtrim(string& s);
+	static void trim(string& s);
 	Manager();
 	~Manager();
 };
